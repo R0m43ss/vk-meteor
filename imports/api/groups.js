943f6c0d873ec66ca2826eb4d9cdd610d3b4a511
R0m43ss <driver-x@sibmail.com>
2016-08-23 21:18:11 +0700

    Small fixes

--- imports/api/groups.js
+++ imports/api/groups.js
@@ -9,6 +9,15 @@ if(Meteor.isServer){
 }
 
 Meteor.methods({
+	// Insert test data
+	'groups.data'(){
+		if(!Groups.findOne({ group: 72495085 }))
+			Groups.insert({ group: 72495085, postsCount: 9, updTime: 4 });
+		if(!Groups.findOne({ group: 105256967 }))
+			Groups.insert({ group: 105256967, postsCount: 4, updTime: 2 });
+		if(!Groups.findOne({ group: 3305 }))
+			Groups.insert({ group: 3305, postsCount: 3, updTime: 4 });
+	},
 	'groups.update'(id, gid, count, time){
 		Groups.update(id, { $set: { group: gid, postsCount: count, updTime: time } });
 	},
--- imports/ui/body.js
+++ imports/ui/body.js
@@ -5,7 +5,8 @@ import { Posts } from '../api/posts.js';
 import './body.html';
 import './post.js';
 
-var length = 1, index = 0, interval_id;
+var index = 0, interval_id;
+const options = {redesign: 1, mode: 3, width: "auto", height: "auto", color1: 'FFFFFF', color2: '000000', color3: '5E81A8'};	// Options for VK widget
 
 Template.body.onCreated(function bodyOnCreated() {
 	this.state = new ReactiveDict();
@@ -13,7 +14,7 @@ Template.body.onCreated(function bodyOnCreated() {
 	index = this.state.get('Index');
 	Meteor.subscribe('posts');
 	Meteor.subscribe('groups', function(){
-		length = Groups.find().count();
+		Meteor.call('groups.data');
 		updatePosts();
 	});
 });
@@ -23,101 +24,110 @@ Template.body.helpers({
 		return Posts.find({});
 	},
 	groupInfo() {
-		return VK.Widgets.Group("vk_groups",{redesign: 1, mode: 3, width: "auto", height: "auto", color1: 'FFFFFF', color2: '000000', color3: '5E81A8'},Groups.find().fetch()[Template.instance().state.get('Index')].group);
+		let group_id = Groups.find().fetch()[Template.instance().state.get('Index')].group;
+		return VK.Widgets.Group("vk_groups", options, group_id);
 	},
 });
 
 Template.body.events({
 	// Go to the previous item of communities list
 	'click .prevButton'(){
-		document.getElementById('vk_groups').innerHTML = "";
+		$("#vk_groups").html("");
 		if(Template.instance().state.get('Index') == 0)
 			// If it's the beginning of list, go to the last item
-			Template.instance().state.set('Index', length - 1);
+			Template.instance().state.set('Index', Groups.find().count() - 1);
 		else {
 			// Otherwise go to the previous item
-			var oldIndex = Template.instance().state.get('Index');
+			let oldIndex = Template.instance().state.get('Index');
 			Template.instance().state.set('Index', oldIndex - 1);
 		}
 		index = Template.instance().state.get('Index');
 		updatePosts();
-		document.getElementById('gid').value = Groups.find().fetch()[Template.instance().state.get('Index')].group;
-		document.getElementById('count').value = Groups.find().fetch()[Template.instance().state.get('Index')].postsCount;
-		document.getElementById('upd').value = Groups.find().fetch()[Template.instance().state.get('Index')].updTime;
+		$("#gid").val(Groups.find().fetch()[index].group);
+		$("#count").val(Groups.find().fetch()[index].postsCount);
+		$("#upd").val(Groups.find().fetch()[index].updTime);
 	},
 	// Go to the next item of community list
 	'click .nextButton'(){
-		document.getElementById('vk_groups').innerHTML = "";
-		if(Template.instance().state.get('Index') == length - 1)
+		$("#vk_groups").html("");
+		if(Template.instance().state.get('Index') == Groups.find().count() - 1)
 			// If it's the end of list, go to the first item
 			Template.instance().state.set('Index', 0);
 		else {
 			// Otherwise go to the next item
-			var oldIndex = Template.instance().state.get('Index');
+			let oldIndex = Template.instance().state.get('Index');
 			Template.instance().state.set('Index', oldIndex + 1);
 		}
 		index = Template.instance().state.get('Index');
 		updatePosts();
-		document.getElementById('gid').value = Groups.find().fetch()[Template.instance().state.get('Index')].group;
-		document.getElementById('count').value = Groups.find().fetch()[Template.instance().state.get('Index')].postsCount;
-		document.getElementById('upd').value = Groups.find().fetch()[Template.instance().state.get('Index')].updTime;
+		$("#gid").val(Groups.find().fetch()[index].group);
+		$("#count").val(Groups.find().fetch()[index].postsCount);
+		$("#upd").val(Groups.find().fetch()[index].updTime);
 	},
 	// Open/close form for editing community data
 	'click .editButton'(){
-		if(document.getElementById('groupForm').style.display === "none") {
-			document.getElementById('groupForm').style.display = "block";
-			document.getElementById('gid').value = Groups.find().fetch()[Template.instance().state.get('Index')].group;
-			document.getElementById('count').value = Groups.find().fetch()[Template.instance().state.get('Index')].postsCount;
-			document.getElementById('upd').value = Groups.find().fetch()[Template.instance().state.get('Index')].updTime;
+		if($("#groupForm").css("display") === "none") {
+			let i = Template.instance().state.get('Index');
+			$("#groupForm").css("display", "block");
+			$("#gid").val(Groups.find().fetch()[i].group);
+			$("#count").val(Groups.find().fetch()[i].postsCount);
+			$("#upd").val(Groups.find().fetch()[i].updTime);
 		}
 		else {
-			document.getElementById('groupForm').style.display = "none";
-			document.getElementById('gid').value = "";
-			document.getElementById('count').value = "";
-			document.getElementById('upd').value = "";
+			$("#groupForm").css("display", "none");
+			$("#gid").val("");
+			$("#count").val("");
+			$("#upd").val("");
 		}
 	},
 	// Save changes of community data
 	'click .saveEditButton'(){
-		if(!parseInt(document.getElementById('gid').value,10) || !parseInt(document.getElementById('count').value,10) || !parseInt(document.getElementById('upd').value,10))
-		{
-			throw new Meteor.Error("Wrong data format");
+		let g_id = Groups.find().fetch()[Template.instance().state.get('Index')]._id;
+		let gid = Math.abs(parseInt($("#gid").val(),10));
+		let num = Math.abs(parseInt($("#count").val(),10));
+		let updTime = Math.abs(parseInt($("#upd").val(),10));
+		if(isNaN(gid) || (isNaN(num)) || isNaN(updTime)){
+			throw new Meteor.Error("Wrong data format: Integer expected");
 		}
-		var g_id = Groups.find().fetch()[Template.instance().state.get('Index')]._id;
-		var gid = Math.abs(parseInt(document.getElementById('gid').value,10));
-		var num = Math.abs(parseInt(document.getElementById('count').value,10));
-		var updTime = Math.abs(parseInt(document.getElementById('upd').value,10));
-		if(gid != Groups.findOne(g_id).group || num != Groups.findOne(g_id).postsCount || updTime != Groups.findOne(g_id).updTime){
-			document.getElementById('vk_groups').innerHTML = "";
+		let g = Groups.findOne(g_id);
+		if(gid != g.group || num != g.postsCount || updTime != g.updTime){
+			$("#vk_groups").html("");
 		}
 		Meteor.call('groups.update', g_id, gid, num, updTime);
 		index = Template.instance().state.get('Index');
 		updatePosts();
-		document.getElementById('groupForm').style.display = "none";
+		$("#groupForm").css("display", "none");
 	},
 });
 
 // Update posts list
 function updatePosts() {
 	if(Meteor.isClient) {
-		var id = Groups.find().fetch()[index].group;
-		var count = Groups.find().fetch()[index].postsCount;
-		var upd = Groups.find().fetch()[index].updTime;
+		let id = Groups.find().fetch()[index].group;
+		let count = Groups.find().fetch()[index].postsCount;
+		let upd = Groups.find().fetch()[index].updTime;
 		Meteor.call('posts.removeAll');
 		Meteor.call('posts.get', id, count, function(err, res) {
-			var str, post, pic;
-			for(var i=1; i < $.parseJSON(res.content).response.length; i++) {
-				str = $.parseJSON(res.content).response[i].text.replace(/<br>/g,"\n");
-				post = $.parseJSON(res.content).response[i].id;
-				pic="";
-				if($.parseJSON(res.content).response[i].attachment) {
-					if($.parseJSON(res.content).response[i].attachment.type=="photo")
-						pic = $.parseJSON(res.content).response[i].attachment.photo.src_big;
+			if(!err) {
+				let ans = $.parseJSON(res.content);
+				for(var i = 1; i < ans.response.length; i++) {
+					let str = ans.response[i].text.replace(/<br>/g,"\n");
+					let post = ans.response[i].id;
+					let pic="";
+					if(ans.response[i].attachment) {
+						if(ans.response[i].attachment.type=="photo")
+							pic = ans.response[i].attachment.photo.src_big;
+					}
+					Posts.insert({ text: str , url: 'http://vk.com/feed?w=wall' + id*(-1) + '_' + post, img: pic, });
 				}
-				Posts.insert({ text: str , url: 'http://vk.com/feed?w=wall' + id*(-1) + '_' + post, img: pic, });
+			}
+			else {
+				console.log(err);
 			}
 		});
-		Meteor.clearInterval(interval_id);
+		if(interval_id) {
+			Meteor.clearInterval(interval_id);
+		}
 		interval_id = Meteor.setInterval(updatePosts, upd*60000);
 	}
 }
